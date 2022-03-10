4d227a97d2b239bf63d53f5e8b646e03f59a9ed7
Tim <Tim@LUKE-LAPTOP>
2022-03-10 09:50:47 +0100

    Projektdateien hinzufügen.

---
+++ Kapitalrechner/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Zinsrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Jahr;
+
+            double Anfangskapital;
+            int Prozentsatz;
+
+
+
+
+            Console.WriteLine("Geben Sie das Startkapital ein.");
+            Anfangskapital = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geben Sie den Zinssatz ein.");
+            Prozentsatz = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geben Sie die Jahre ein.");
+            Jahr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Jahr Guthaben Zinsen K.Zinsen Bilanz");
+
+            Console.WriteLine("Jahr    Guthaben    Zinsen   K.Zinsen    Bilanz");
+            #region Jahre
+            
+            for (int i = 1; i <= Jahr; i++)
+            {
+                Console.WriteLine("{0,3}", i);
+            }
+            Console.ReadKey();
+            #endregion
+           
+            
+            #region Guthaben
+            for (int  a= 1; a==Anfangskapital; a++)
+            {
+                Console.WriteLine("{0,4}", a);
+            }
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
---
+++ RECHNER/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Rechner
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Geben sie das Kapital ein");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben sie den Prozentsatz ein");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben sie die Anzahl der Jahre ein");
+            double n = Convert.ToDouble(Console.ReadLine());
+          
+           
+            Console.WriteLine($"{"Jahr",4}  {"Guthaben",11}  {"Zinsen",9}  {"K.Zinsen",9}  {"Billanz",9}");
+            Console.WriteLine("--------------------------------------------------");
+
+            double nextcapital = 0.00;
+            double ZinsAkt = 0.00;
+            double Zinssumme = 0.00;
+            double Kapitaljahr = 0.00;
+            nextcapital = k;
+
+
+                
+
+
+            for (int i = 1; i<=n;i++)
+            {
+                
+
+                ZinsAkt = nextcapital * (p/100);
+                Zinssumme = Zinssumme + ZinsAkt;
+                Kapitaljahr = nextcapital+Zinssumme;
+                nextcapital = nextcapital + ZinsAkt;
+
+                if (i == n)
+                {
+                    Console.WriteLine("--------------------------------------------------");
+                }
+
+
+
+                Console.WriteLine($"{i,4}|{nextcapital,11:F2}E|{ZinsAkt,9:F2}E|{Zinssumme,9:F2}E|{Kapitaljahr,9:F2}E");
+              
+            }
+            Console.ReadKey();
+        }
+    }
+}
